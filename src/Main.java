2d9083768cc7dbf605d8dec972bec215cfd041c9
Sydney Hutton <huttonsy@mail.uc.edu>
2023-02-07 13:58:56 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String[] args) {
+        // intOperandA, intOperandB, intSum, intProduct, intDifference, intQuotient, intModulo
+
+        int intOperandA = 2;
+        int intOperandB = 5;
+
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo;
+
+        intSum = intOperandA + intOperandB;
+
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intSum);
+    }
+}
+class Task2 {
+    public static void main(String[] args) {
+            int costLunch = 10;
+            int doubleLunch = 20;
+            System.out.println("My lunch cost is: " + costLunch) ;
+            int kidsFam = 2;
+            System.out.println("Kids in my family: " + kidsFam);
+            boolean isRaining;
+                isRaining = false;
+                    System.out.println("It is not raining");
+            int gallonGas = 3;
+            System.out.println("A gallon of gas costs: " + gallonGas);
+            int favNum = 2;
+            System.out.println("My favorite number is: " + favNum);
+            int shoeSize = 7;
+            System.out.println("My shoe size is: " + shoeSize);
+            int birthMonth = 2;
+            System.out.println("My birth month is: " + birthMonth);
+            String fullName = "Sydney Hutton" ;
+            System.out.println("My full name is: " + fullName);
+
+            doubleLunch = costLunch*2;
+            System.out.println("The cost of my lunch is: " + costLunch );
+            System.out.println("The cost of double my lunch is: " + doubleLunch );
+
+
+        }
+    }
+     
+     
